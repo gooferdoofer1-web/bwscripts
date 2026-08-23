da2553d5cfc288b6ed5dbb392257ab57526ca1aa
gooferdoofer1-web <gooferdoofer1@gmail.com>
2026-08-23 14:22:25 -0400

    Update customHey.js
    why tf did i do socket.once()

--- customHey.js
+++ customHey.js
@@ -1,13 +1,9 @@
 function hey(who,text) {
 	socket.emit("talk",{text: `Hey, ${nisolate(who)}!`});
-	socket.once("talk",data => {
-		if (data.text === `Hey, ${nisolate(who)}!`) {
-			speak.play(`Hey, ${markdownToSpeech(who)}!`,{
-				speed: usersPublic.get(me).speed,
-				pitch: usersPublic.get(me).pitch
-			},()=>{
-				socket.emit("talk",{text})
-			})
-	}
-})
+	speak.play(`Hey, ${markdownToSpeech(who)}!`,{
+		speed: usersPublic.get(me).speed,
+		pitch: usersPublic.get(me).pitch
+	},()=>{
+		socket.emit("talk",{text})
+	})
 }
